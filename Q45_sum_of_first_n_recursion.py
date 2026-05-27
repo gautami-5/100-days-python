edfa443d12c3a7e3f5b2d6b3e05d0233046e49ed
Gautami.K <gautamik9191@gmail.com>
2026-05-27 23:17:01 +0530

    sum of first n natural numbers using recursion

---
+++ Q45_sum_of_first_n_recursion.py
@@ -0,0 +1,20 @@
+"""
+sum(1) = 1
+sum(2) = 1 + 2
+sum(3) = 1 + 2 + 3
+.
+.
+sum(n) = 1 + . . + n-1 + n
+sum(n) = sum(n-1) + n
+
+"""
+
+
+
+def sum(n):
+    if n ==1:
+        return 1
+    return sum(n-1) + n
+
+n = int(input("Enter a number: "))
+print(f"The sum of first {n} natural numbers is: {sum(n)}")
