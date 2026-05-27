425294a74b02f4c74e4e42924bad4a71a4a4f4e4
Gautami.K <gautamik9191@gmail.com>
2026-05-27 23:18:26 +0530

    Multiplication Table using function

---
+++ Q48_multiplication_table_functions.py
@@ -0,0 +1,10 @@
+def mul(n):
+    for x in range(1,11):
+     print(f"{n} x {x} = {n*x}")
+    
+    
+n = int(input("Enter number for multiplication table: "))
+print(f"{n} Multiplication Table \n")
+mul(n)
+    
+
